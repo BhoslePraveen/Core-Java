2c23a9bac38e1f90287dfde40606761d873e07ba
Praveen Bhosle <praveen.bhosle@sarvagram.com>
2025-04-28 08:13:53 +0530

    Added Employee class

---
+++ src/com/velocity/demo/Employee.java
@@ -0,0 +1,41 @@
+package com.velocity.demo;
+
+public class Employee {
+	private String empName;
+	private String address;
+	private int age;
+	private double salary;
+
+	public Employee(String empName, String address, int age, double salary) {
+		super();
+		this.empName = empName;
+		this.address = address;
+		this.age = age;
+		this.salary = salary;
+	}
+	
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+}
