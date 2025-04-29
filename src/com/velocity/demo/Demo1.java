656642598feafcf0df9d58126c8178ac186de7fb
Praveen Bhosle <praveen.bhosle@sarvagram.com>
2025-04-29 07:21:54 +0530

    Added List iterator example

---
+++ src/com/velocity/demo/Demo1.java
@@ -0,0 +1,22 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		List<Employee> employees = new ArrayList<Employee>();
+		employees.add(new Employee("Ravi", "Pune", 26, 150000));
+		employees.add(new Employee("Vikas", "Mumbai", 27, 160000));
+		employees.add(new Employee("Reena", "Nashik", 28, 170000));
+
+		ListIterator<Employee> itr = employees.listIterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+	}
+
+}
