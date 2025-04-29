b13ff2e53554417d0d6d9f1de24fe2452ae00062
Praveen Bhosle <praveen.bhosle@sarvagram.com>
2025-04-29 07:11:25 +0530

    Added Test class

---
+++ src/com/velocity/demo/Test.java
@@ -0,0 +1,18 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<Employee> employees = new ArrayList<Employee>();
+		employees.add(new Employee("Ravi", "Pune", 26, 150000));
+		employees.add(new Employee("Vikas", "Mumbai", 27, 160000));
+		employees.add(new Employee("Reena", "Nashik", 28, 170000));
+
+		System.out.println(employees);
+
+	}
+
+}
