0f1560b9fdc0bb0cc2df94e3a00d178acd159253
BhoslePraveen <111246105+BhoslePraveen@users.noreply.github.com>
2025-05-05 07:54:04 +0530

    Removed in active emp

--- src/com/velocity/demo/Test.java
+++ src/com/velocity/demo/Test.java
@@ -8,12 +8,11 @@ public class Test {
 	public static void main(String[] args) {
 		List<Employee> employees = new ArrayList<Employee>();
 		employees.add(new Employee("Ravi", "Pune", 26, 150000));
-		employees.add(new Employee("Vikas", "Mumbai", 27, 160000));
 		employees.add(new Employee("Reena", "Nashik", 28, 170000));
 		employees.add(new Employee("Shonu", "Hyderbad", 27, 180000));
 
 		for (Employee emp : employees) {
-			System.out.println(emp);
+		    System.out.println("Emp Detais are : "+emp);
 		}
 
 	}
