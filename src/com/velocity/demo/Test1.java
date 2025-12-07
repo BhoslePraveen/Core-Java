af26917833903fbc85813ee3c2a1128bc1c33ab8
praveen <praveen.bhosle5@gmail.com>
2025-12-07 08:08:48 +0530

    Refactored the code as per comments

--- src/com/velocity/demo/Test1.java
+++ src/com/velocity/demo/Test1.java
@@ -1,16 +1,19 @@
 package com.velocity.demo;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Test1 {
 
 	public static void main(String[] args) {
-		ArrayList<Student> list = new ArrayList<Student>();
-		list.add(new Student(101, "Vishal", "11", "Pune"));
-		list.add(new Student(102, "Abhay", "11", "Mumbai"));
-		list.add(new Student(103, "Depali", "11", "Bangalore"));
-		
-		System.out.println(list);
+		List<Student> students = new ArrayList<Student>();
+		students.add(new Student(101, "Vishal", "11", "Pune"));
+		students.add(new Student(102, "Abhay", "11", "Mumbai"));
+		students.add(new Student(103, "Depali", "11", "Bangalore"));
+
+		for (Student stu : students) {
+			System.out.println(stu);
+		}
 
 	}
 
